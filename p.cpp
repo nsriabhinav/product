8f29b8c3753a2e4f1808c4151b0f7f9438a1bf66
nsriabhinav <64884634+nsriabhinav@users.noreply.github.com>
2020-10-05 10:27:12 +0530

    Add files via upload

---
+++ p.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,p;
+	cout<<"enter x";
+	cin>>x;
+	cout<<"\nenter y";
+	cin>>y;
+	p=x*y;
+	cout<<"\n product is:"<<p;
+	return 0;
+}
